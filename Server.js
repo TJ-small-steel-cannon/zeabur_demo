fda1ff08f879ac795ec6fefb87279a302f985bff
xiao_gang_pao <quanquan>
2023-04-01 12:18:50 +0800

    first commit

---
+++ Server.js
@@ -0,0 +1,16 @@
+const express=require('express')
+const app = express()
+
+app.get('/students',(request,response)=>{
+	const students = [
+		{id:'001',name:'tom',age:18},
+		{id:'002',name:'jerry',age:19},
+		{id:'003',name:'tony',age:120},
+	]
+	response.json(students)
+})
+
+app.listen(5000,(err)=>{
+	if(!err) console.log('服务器1启动成功了,请求学生信息地址为：http://localhost:5000/students');
+})
+
